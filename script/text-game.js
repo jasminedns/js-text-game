8337c554ce9f6ce6c1462983c73a2146dc959374
Jasminedns <jasminedanese@icloud.com>
2024-11-04 10:01:02 +0100

    new code, changed push to unshift

--- script/text-game.js
+++ script/text-game.js
@@ -1,24 +1,26 @@
 let userHealth = 20;
 let ogreHealth = 20;
 let rolls = [];
-const MINOR_DAMAGE = 3;
-const MAJOR_DAMAGE = 6;
+const MINOR_DAMAGE = 2;
+const MAJOR_DAMAGE = 4;
 let winner = false;
-alert("Welcome to Dungeons and Dragons! Are you ready to fight and kill the ogre that has been killing all the travellers around your town? Here's how it'll work: every time you do something the computer will roll a 20 sided dice.. Just hope that you get more than 10 and you'll be fine. Let's try it out before starting the game. Press OK.");
+alert(`Welcome to Dungeons and Dragons! Are you ready to fight and kill the ogre that has been killing people around your town? 
+    Here's how it'll work: every time you do something the computer will roll a 20 sided dice.. Just hope that you get more than 10 and you'll be fine. 
+    Let's try it out before starting the game. Press OK.`);
 
 let userRoll = Math.floor(Math.random() * 20) + 1;
-
-rolls.push(userRoll);
+rolls.unshift(userRoll);
 
 if (userRoll <= 10) {
     alert(`You rolled ${userRoll}, we're not starting very well.. Hope it goes better when you're in-game for your own safety!`)
 } else {
-    alert(`Damn! A ${userRoll} already? Let's hope you keep rolling like this later`)
+    alert(`Damn! A ${userRoll} already? Let's hope you keep rolling like this later!`)
 }
 
-alert("So... Where were we? Yes... You are a warrior magician that decided that today's the day for the ogre to stop messing with the people around your village. So you take the golden path to the forest in order to find him and slay him. You see him right outside a cave while he's eating who-knows-what. You are aeger to strike him at once and when he doesn't even expect it. Roll to see if you succed to attack him!")
+alert("So... Where were we? Yes... You are an elf warrior that decided that today's the day for the ogre to stop messing with the people around your village. So you take the golden path to the forest in order to find and slay him. You see him right outside a cave while he's eating God-knows-what. You are aeger to strike him at once and when he doesn't even expect it. Roll to see if you succed to attack him!")
 
 userRoll = Math.floor(Math.random() * 20) + 1;
+rolls.unshift(userRoll);
 
 if (userRoll <= 10) {
     alert(`You rolled ${userRoll}... You tried to attack him but as soon as you try to strike, he gets up and slaps you to the ground! The fight starts! Your health: ${userHealth - MINOR_DAMAGE}/20 -- Ogre's health: ${ogreHealth}/20`)
@@ -28,24 +30,27 @@ if (userRoll <= 10) {
     ogreHealth = ogreHealth - MINOR_DAMAGE;
 }
 
-
 while (!winner) {
-    if (userHealth > 0 && ogreHealth > 0) { //works!! BUT SHOWS NEGATIVE VALUE FIRST AND THEN EXITS THE LOOP (maybe change damange to 2 and 4)
+    if (userHealth > 0 && ogreHealth > 0) { //works!! BUT SHOWS NEGATIVE VALUE FIRST AND THEN EXITS THE LOOP 
         let userInput = prompt(`Type ATTACK (to attack), FLEE (to leave the fight) or CHAT (to resonate with the ogre). After each action the dice will roll and we'll see what happens. 
         Your rolls (most recent first): ${rolls.join(", ")}.
         Your health: ${userHealth} -- ogre's health: ${ogreHealth} Choose your action!`);
         if (userInput === "ATTACK") {
             userRoll = Math.floor(Math.random() * 20) + 1;
+            rolls.unshift(userRoll);
             if (userRoll <= 10) {
-                alert(`Well... at least you tried, right? You rolled a ${userRoll}...  You tried to swing your sword at him but you lose your balance and only scratch him. Your health: ${userHealth}/20 -- Ogre's health: ${ogreHealth - MINOR_DAMAGE}/20 `)
+                alert(`Well... at least you tried, right? You rolled a ${userRoll}...  You tried to swing your sword at him but you lose your balance and only scratch him. 
+                    Your health: ${userHealth}/20 -- Ogre's health: ${ogreHealth - MINOR_DAMAGE}/20 `)
                 ogreHealth = ogreHealth - MINOR_DAMAGE
 
             } else {
-                alert(`Yes! You rolled a ${userRoll}! You take a jump and strike the ogre that falls to the ground and starts bleeding! Your health: ${userHealth}/20 -- Ogre's health: ${ogreHealth - MAJOR_DAMAGE}/20 `)
+                alert(`Yes! You rolled a ${userRoll}! You take a jump and strike the ogre that falls to the ground and starts bleeding! 
+                    Your health: ${userHealth}/20 -- Ogre's health: ${ogreHealth - MAJOR_DAMAGE}/20 `)
                 ogreHealth = (ogreHealth - MAJOR_DAMAGE);
             }
         } else if (userInput === "FLEE") {
             userRoll = Math.floor(Math.random() * 20) + 1;
+            rolls.unshift(userRoll);
             if (userRoll <= 10) {
                 alert(`Well... at least you tried, right? You rolled a ${userRoll}...  You tried to flee from the fight but the ogre grabs you right back into it and attacks you. Your health: ${userHealth - MAJOR_DAMAGE}/20 -- Ogre's health: ${ogreHealth}/20 `)
                 userHealth = userHealth - MAJOR_DAMAGE
@@ -56,6 +61,7 @@ while (!winner) {
             }
         } else if (userInput === "CHAT") {
             userRoll = Math.floor(Math.random() * 20) + 1;
+            rolls.unshift(userRoll);
             if (userRoll <= 10) {
                 alert(`Well... at least you tried, right? You rolled a ${userRoll}...  You try to resonate with the ogre but you unconsciusly say something about his mother. He then grabs you and throws you against a tree. Your health: ${userHealth - MAJOR_DAMAGE}/20 -- Ogre's health: ${ogreHealth}/20 `)
                 userHealth = userHealth - MAJOR_DAMAGE
@@ -67,7 +73,6 @@ while (!winner) {
         } else {
 
         }
-        rolls.push(userRoll);
     } else {
         if (userHealth <= 0) {
             alert(`You died.. sorry!`)
